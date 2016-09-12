d4467c7bda565fbe47a3a0418b23005ace9dbf9a
Margular <2609135351@qq.com>
2016-09-12 21:43:20 +0800

    爬图片

---
+++ picture/picture/spiders/jandan.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from picture.items import PictureItem
+
+class JandanSpider(scrapy.Spider):
+    name = 'jandan'
+    allowed_domains = ['jandan.net']
+    start_urls = ['http://jandan.net/ooxx']
+
+    custom_settings = {
+                'FILES_STORE' : '/home/cui/图片/jandan'
+            }
+
+
+    def parse(self , response):
+        item = PictureItem()
+        item['file_urls'] = response.xpath("//a[@class='view_img_link']/@href").extract()
+        yield item
+        next_url = response.xpath("//a[@class='previous-comment-page']/@href").extract()[0]
+        yield scrapy.Request(next_url , self.parse)
