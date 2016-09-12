b054eaa091a1247e703d4a26a785195fe2f9abb6
Margular <2609135351@qq.com>
2016-09-12 22:21:30 +0800

    yande.re

---
+++ picture/picture/spiders/yande.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from picture.items import PictureItem
+
+
+class YandeSpider(scrapy.Spider):
+    name = "yande"
+    allowed_domains = ["yande.re"]
+    start_urls = (
+        'https://yande.re/post?tags=rating:e&page=1',
+    )
+
+    custom_settings = {
+                'FILES_STORE' : '/home/cui/图片/yande'
+            }
+
+    def parse(self, response):
+        item = PictureItem()
+        item['file_urls'] = response.css("a.directlink.largeimg::attr('href')").extract()
+        yield item
+        next_url = response.urljoin(response.css("a.next_page::attr('href')").extract()[0])
+        yield scrapy.Request(next_url , self.parse)
