3681a5001daef4eff69b377619febb434c854d04
Margular <2609135351@qq.com>
2016-10-11 07:47:36 +0800

    修改了视频保存的名字

--- video/video/pipelines.py
+++ video/video/pipelines.py
@@ -4,8 +4,37 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from scrapy.pipelines.files import FilesPipeline
 
 class VideoPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class MyFilesPipeline(FilesPipeline):
+    def file_path(self, request, response=None, info=None):
+        ## start of deprecation warning block (can be removed in the future)
+        def _warn():
+            from scrapy.exceptions import ScrapyDeprecationWarning
+            import warnings
+            warnings.warn('FilesPipeline.file_key(url) method is deprecated, please use '
+                          'file_path(request, response=None, info=None) instead',
+                          category=ScrapyDeprecationWarning, stacklevel=1)
+
+        # check if called from file_key with url as first argument
+        if not isinstance(request, Request):
+            _warn()
+            url = request
+        else:
+            url = request.url
+
+        # detect if file_key() method has been overridden
+        if not hasattr(self.file_key, '_base'):
+            _warn()
+            return self.file_key(url)
+        ## end of deprecation warning block
+
+        media_guid = hashlib.sha1(to_bytes(url)).hexdigest()  # change to request.url after deprecation
+        print("media_guid : %s" % media_guid)
+        media_ext = os.path.splitext(url)[1]  # change to request.url after deprecation
+        print("media_ext : %s" % media_ext)
+        return 'full/%s%s' % (media_guid, media_ext)
