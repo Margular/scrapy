b522a0e1f4fdadc3be0d28310d0dd0819e65367b
Margular <2609135351@qq.com>
2016-10-11 07:48:26 +0800

    不要代理了

--- video/video/middlewares.py
+++ video/video/middlewares.py
@@ -1,3 +1,4 @@
 class ProxyMiddleware(object):
     def process_request(self, request, spider):
-        request.meta['proxy'] = "http://127.0.0.1:8118"
+        #request.meta['proxy'] = "http://127.0.0.1:8099"
+        pass
