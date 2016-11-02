196c6607645dc16c64ecd32b1e902fb019909068
Margular <2609135351@qq.com>
2016-11-02 13:18:06 +0800

    修改LOG_LEVEL为DEBUG

--- video/video/settings.py
+++ video/video/settings.py
@@ -106,4 +106,5 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
-LOG_LEVEL = 'INFO'
+
+LOG_LEVEL = 'DEBUG'
